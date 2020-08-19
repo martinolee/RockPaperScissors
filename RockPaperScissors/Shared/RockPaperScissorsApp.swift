93f0f6a0890670b16fe1360fa9d454a52765fefe
martinolee <soohan.m.lee@gmail.com>
2020-08-19 22:14:41 +0900

    Add initial project

---
+++ RockPaperScissors/Shared/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  Shared
+//
+//  Created by Soohan Lee on 2020/08/18.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
